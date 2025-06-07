35ee4a3a8cd780b0d4352d8aaf175e6cb79359eb
pepelax <mzaprudin@gmail.com>
2025-06-07 17:41:00 +0500

    [NEW] Docker setup

---
+++ docker-setup.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker.io docker-compose
+
+sudo apt install -y python3-setuptools
+sudo apt install -y python3-distutils
+
+sudo systemctl start docker
+sudo systemctl enable docker
