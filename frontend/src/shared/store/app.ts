1076e7bee7361ea69bce1afdde0b25767cc2cbdd
Polishchuk <staspol216@gmail.com>
2025-06-08 13:02:12 +0300

    wip

--- frontend/src/shared/store/app.ts
+++ frontend/src/shared/store/app.ts
@@ -44,7 +44,7 @@ export const useSummaryStore = defineStore("summary", () => {
   async function getSumaryBySettings(
     settings: Settings = { sectors: [], tickers: [], style: "temperate" }
   ) {
-    const query = { agent: "news_analyzer_json" };
+    const query = { agent: "test_news_analyzer_retriver" };
 
     const data = {
       messages: [
@@ -61,7 +61,7 @@ export const useSummaryStore = defineStore("summary", () => {
 
     try {
       const res = await instance.post(
-        `${import.meta.env.VITE_API_URL}/run_agent`,
+        `${import.meta.env.VITE_API_URL}/run_agent_retriver`,
         data,
         { params: query }
       );
